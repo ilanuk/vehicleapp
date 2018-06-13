835f69d833aef57c245d4c42dd94d8a3e0f2b48d
Ilan Balasundaram <ilanuk@yahoo.com>
2018-06-13 09:51:34 -0400

    Update the last delete functionality

--- src/main/java/com/company/repository/VehicleController.java
+++ src/main/java/com/company/repository/VehicleController.java
@@ -1,13 +1,14 @@
 package com.company.repository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,8 +29,9 @@ public class VehicleController {
     public ResponseEntity<Void> deleteLastVehicle() {
         List<Vehicle> vehicles = vehicleJpaRepository.findAll();
         if(!vehicles.isEmpty()) {
-        	Long lastId = vehicles.get(vehicles.size()-1).getId();
-        	vehicleJpaRepository.deleteById(lastId);
+        	OptionalInt lastId = vehicles.stream().mapToInt(veh->veh.getId().intValue())
+        			.max();
+        	vehicleJpaRepository.deleteById((long) lastId.getAsInt());
         }
         return ResponseEntity.ok().build(); 
     }
